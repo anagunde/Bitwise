41f659f31f4fefe6800ef63c6a552b9dd3240fcc
Durga Anagunde <anagunde2003@gmail.com>
2025-05-01 19:19:41 +0530

    Eligable To Vote

---
+++ Age.java
@@ -0,0 +1,21 @@
+package EligableToVote;
+
+public class Age {
+	public static void main(String args[])
+	{
+		int Age = 15;
+	  // System.out.println(Student  Eligiable for vote = "+(20<=Age));
+	 //System.out.println(Student Eligiable for vote = "+(20>=Age));
+		
+		if(Age>=20)
+		{
+			System.out.println("Student Eligiable for vote");
+		}
+		else
+		{
+			System.out.println("Student  Not Eligiable for vote");
+		}
+	}
+
+}
+// Output :- Student Not Eligiable for vote  
