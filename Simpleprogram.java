f483069092ab532eb712b0de628f72af2bce94d2
Durga Anagunde <anagunde2003@gmail.com>
2025-05-01 19:00:21 +0530

    Demo Class

---
+++ Simpleprogram.java
@@ -0,0 +1,10 @@
+package DemoClass;
+
+public class Simpleprogram {
+	public static void main(String args[])
+	{
+		System.out.println("Hello,Good Morning");
+		System.out.println("Nice To Meet You");
+	}
+
+}
